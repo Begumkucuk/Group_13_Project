c834a11b874b2f0404e1e4f5abba6b5b8d93cc08
yusufkaya24 <88247454+yusufkaya24@users.noreply.github.com>
2021-10-04 22:14:00 -0400

    Revert "Develop"

--- src/setup/DevransFile.java
+++
@@ -1,7 +0,0 @@
-package setup;
-
-public class DevransFile {
-    public static void main(String[] args) {
-        System.out.println("this is going the dev branch first");
-    }
-}
--- src/setup/YanasFile2.java
+++
@@ -1,7 +0,0 @@
-package setup;
-
-public class YanasFile2 {
-    public static void main(String[] args) {
-        // this is second try lol
-    }
-}
